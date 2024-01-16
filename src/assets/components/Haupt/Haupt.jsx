a0fc475d2d37ebf3bad32160856dcd5bf1dfce34
Farhad <149811241+Far1had@users.noreply.github.com>
2024-01-16 15:13:24 +0100

    FA

---
+++ src/assets/components/Haupt/Haupt.jsx
@@ -0,0 +1,11 @@
+
+const Haupt = () => {
+    return ( 
+        <>
+        <h1>das ist ein Haupt component</h1>
+       
+        </>
+     );
+}
+ 
+export default Haupt;
---
+++ src/assets/components/header/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+    return ( 
+
+        <>
+        <div>
+            <h1>JS.</h1>
+            <nav>
+                <ul>
+                    <li><a href="projects">projects</a></li>
+                    <li><a href="skills">skills</a></li>
+                    <li><a href="contact">contact</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        
+        </>
+     );
+}
+ 
+export default Header;
